7896099397925913894804138a4096007a58f9fe
Vishal <76504332+Vish501@users.noreply.github.com>
2025-06-27 19:37:46 +0000

    Updated file to include Logger functionality

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,23 @@
+# Logger constructed to track any issues with the code during deployment
+
+import os
+import sys
+import logging
+
+logging_string = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_string,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
