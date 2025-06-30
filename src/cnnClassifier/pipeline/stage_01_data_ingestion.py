37a38c712a196a157b3439bdb64e3008867741e5
Vishal <76504332+Vish501@users.noreply.github.com>
2025-06-30 20:33:48 +0000

    Updated data ingestion logic for use

---
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,43 @@
+from cnnClassifier.config.configurations import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
+
+from cnnClassifier import get_logger
+
+# Initializing the logger
+logger = get_logger()
+
+# Stage identifier for logging purposes
+STAGE_NAME = "Data Ingestion"
+
+class DataIngestionTrainingPipeline:
+    """
+    Pipeline class to orchestrate the data ingestion process:
+    - Load configuration
+    - Download dataset from Kaggle
+    - Extract dataset contents
+    """
+    @staticmethod
+    def main():
+        config_manager = ConfigurationManager()
+        ingestion_config = config_manager.get_ingestion_config()
+
+        data_ingestor = DataIngestion(config=ingestion_config)
+        data_ingestor.download_files()
+        data_ingestor.extract_files()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
+        
+        # Run the pipeline
+        ingestion_pipeline = DataIngestionTrainingPipeline()
+        ingestion_pipeline.main()
+
+        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
+    
+    except Exception as exception:
+    # Catch and log any unexpected errors during the ingestion stage
+        logger.exception(f"Unexpected error during data ingestion pipeline: {exception}")
+        raise exception
+    
