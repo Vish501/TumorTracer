fec54fde42301e98cafcf084d4a1078f7aeb0831
Vishal <76504332+Vish501@users.noreply.github.com>
2025-06-30 20:37:27 +0000

    Temp code to check if the Data Ingestion pipeline is working

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier import get_logger
 
+# Initializing the logger
 logger = get_logger()
 
-logger.info("Welcome to this project based on cnnClassification!")
+STAGE_NAME = "Data Ingestion"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
+        
+        # Run the pipeline
+        ingestion_pipeline = DataIngestionTrainingPipeline()
+        ingestion_pipeline.main()
+
+        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
+    
+    except Exception as exception:
+        # Catch and log any unexpected errors during the ingestion stage
+        logger.exception(f"Unexpected error during data ingestion pipeline: {exception}")
+        raise exception
