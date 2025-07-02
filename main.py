0e3b008e23bcf9e417cb5e5d2c5c30f045165f12
Vishal <76504332+Vish501@users.noreply.github.com>
2025-07-02 13:09:13 +0000

    Adding Data Ingestion and Base Model pipelines

---
+++ main.py
@@ -0,0 +1,31 @@
+from cnnClassifier.pipeline import DataIngestionTrainingPipeline, BaseModelPipeline
+from cnnClassifier import get_logger
+
+# Initializing the logger
+logger = get_logger()
+
+# Stage identifier for logging purposes
+STAGE_NAME = ""
+
+if __name__ == "__main__":
+    try:
+        # Running the Data Ingestion Pipeline
+        STAGE_NAME = "Data Ingestion"
+        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
+        DataIngestionTrainingPipeline.main() # Run the pipeline
+        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
+        logger.info(f"******************************************")
+
+        # Running the Base Model Pipeline
+        STAGE_NAME = "Base Model"
+        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
+        BaseModelPipeline.main() # Run the pipeline
+        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
+        logger.info(f"******************************************")
+    
+    except Exception as exception:
+        # Catch and log any unexpected errors during the ingestion stage
+        logger.exception(f"Unexpected error during {STAGE_NAME} pipeline: {exception}")
+        raise exception
+    
+    
