f65e044bc3579437cae19a83f52d17243a01cc29
Vishal <76504332+Vish501@users.noreply.github.com>
2025-07-02 13:08:54 +0000

    Moving code to main.py as app.py

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+import flask
 from cnnClassifier import get_logger
 
 # Initializing the logger
 logger = get_logger()
-
-STAGE_NAME = "Data Ingestion"
-
-if __name__ == "__main__":
-    try:
-        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
-        
-        # Run the pipeline
-        ingestion_pipeline = DataIngestionTrainingPipeline()
-        ingestion_pipeline.main()
-
-        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
-    
-    except Exception as exception:
-        # Catch and log any unexpected errors during the ingestion stage
-        logger.exception(f"Unexpected error during data ingestion pipeline: {exception}")
-        raise exception
