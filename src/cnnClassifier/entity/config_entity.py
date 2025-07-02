b3fbfe9e90363d3d377936d5a7e3f957c2d5b88f
Vishal <76504332+Vish501@users.noreply.github.com>
2025-07-02 12:45:05 +0000

    Added BaseModelConfig

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -16,3 +16,19 @@ class DataIngestionConfig:
     kaggle_dataset: str     # The Kaggle dataset identifier "owner/dataset"
     download_zip: Path      # Path where the downloaded ZIP file will be saved
     extracted_file: Path    # Path where the final extracted file will be stored
+
+
+@dataclass(frozen=True)
+class BaseModelConfig:
+    """
+    Immutable configuration class to hold all necessary paths 
+    and parameters required for the base model stage.
+    """
+    root_dir: Path                              # Directory where models will be stored
+    model_path: Path                            # Path to the downloaded base model
+    updated_model_path: Path                    # Path to the updated model with custom layers
+    params_image_size: tuple[int, int, int]     # Input image size, e.g., [224, 224, 3]
+    params_include_top: bool                    # Whether to include fully connected layers
+    params_classes: int                         # Number of output classes
+    params_weights: str                         # Pre-trained weights source
+    params_learning_rate: float                 # Learning rate for training
