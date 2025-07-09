2e04994c4152698ec7d08ee5f446b6863fca7b31
Vishal <76504332+Vish501@users.noreply.github.com>
2025-07-09 11:33:47 +0000

    Updating for predictions

---
+++ src/cnnClassifier/pipeline/predictions.py
@@ -0,0 +1,40 @@
+from cnnClassifier.config.configurations import ConfigurationManager
+from cnnClassifier.components.predictions import Predictions
+from cnnClassifier import get_logger
+
+# Initializing the logger
+logger = get_logger()
+
+# Stage identifier for logging purposes
+STAGE_NAME = "Prediction"
+
+class PredictionPipeline:
+    """
+    Pipeline class to orchestrate the data ingestion process:
+    - Load configuration
+    - Download dataset from Kaggle
+    - Extract dataset contents
+    """
+    @staticmethod
+    def main(image_path):
+        config_manager = ConfigurationManager()
+        prediction_config = config_manager.get_prediction_config()
+
+        prediction_constructor = Predictions(config=prediction_config)
+        prediction_constructor.predict(image_path=image_path)
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
+        
+        # Run the pipeline
+        PredictionPipeline.main(image_path="artifacts/data_ingestion/Data/train/normal/2 - Copy - Copy.png")
+
+        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
+    
+    except Exception as exception:
+        # Catch and log any unexpected errors during the ingestion stage
+        logger.exception(f"Unexpected error during {STAGE_NAME} pipeline: {exception}")
+        raise exception
+    
