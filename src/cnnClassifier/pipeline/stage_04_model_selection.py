ccd6b724dd912e5b959f48d795a16f9997a57d35
Vishal <76504332+Vish501@users.noreply.github.com>
2025-07-11 13:08:18 +0000

    Updated

---
+++ src/cnnClassifier/pipeline/stage_04_model_selection.py
@@ -0,0 +1,35 @@
+from cnnClassifier.config.configurations import ConfigurationManager
+from cnnClassifier.components.model_selection import ModelSelection
+from cnnClassifier import get_logger
+
+# Initializing the logger
+logger = get_logger()
+
+# Stage identifier for logging purposes
+STAGE_NAME = "Model Selection"
+
+class ModelSelectionPipeline:
+    """
+    Pipeline class to selected the best model.
+    """
+    @staticmethod
+    def main():
+        config_manager = ConfigurationManager()
+        selector_config = config_manager.get_model_selector_config()
+        ModelSelection(config=selector_config)
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>> {STAGE_NAME} stage has started <<<<")
+        
+        # Run the pipeline
+        ModelSelectionPipeline.main()
+
+        logger.info(f">>>> {STAGE_NAME} stage has completed <<<<")
+    
+    except Exception as exception:
+        # Catch and log any unexpected errors during the ingestion stage
+        logger.exception(f"Unexpected error during {STAGE_NAME} pipeline: {exception}")
+        raise exception
+    
